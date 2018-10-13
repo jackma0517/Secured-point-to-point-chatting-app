b387480dc7af3ad5dc5232ef2143d688ef15872b
valrat <leri.ratu@gmail.com>
2018-10-12 18:02:04 -0700

    add ip and port config

--- py/ui.py
+++ py/ui.py
@@ -4,6 +4,7 @@
 # Apologies if it offends your sensibilities
 
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 
 # The mode the program is operating in
 class Mode:
@@ -20,24 +21,55 @@ class Application(tk.Frame):
         super().__init__(master)
         self.pack()
         self.state = State()
+        
+        # String Variables
+        self.str_mode = tk.StringVar()
+        self.str_mode.set(self.state.mode)
+
         self.create_widgets()
 
     def create_widgets(self):
+
+        # Frame for Mode toggles and IP/Port
+        self.fr_modes = tk.Frame(self)
+        self.fr_modes.pack()
+
         # Mode Toggle Button
-        self.bt_toggle = tk.Button(self, text='Toggle Mode', command=self.toggle_mode)
+        self.bt_toggle = tk.Button(master=self.fr_modes, text='Toggle Mode', command=self.toggle_mode)
         self.bt_toggle.pack(side='left')
 
         # Mode Text UI
-        self.txt_mode = tk.Text(self)
-        self.txt_mode.pack(side='left')
+        self.lbl_mode = tk.Label(master=self.fr_modes, textvariable=self.str_mode)
+        self.lbl_mode.config(bg=root.cget('bg'), width=15, height=1)
+        self.lbl_mode.pack(side='left')
 
+        self.lbl_ip = tk.Label(master=self.fr_modes, text='IP: ')
+        self.lbl_ip.pack(side='left')
+        self.txt_ip = tk.Text(master=self.fr_modes)
+        self.txt_ip.config(width=15, height=1)
+        self.txt_ip.pack(side='left')
+
+        self.lbl_port = tk.Label(master=self.fr_modes, text='Port: ')
+        self.lbl_port.pack(side='left')
+        self.txt_port = tk.Text(master=self.fr_modes)
+        self.txt_port.config(width=15, height=1)
+        self.txt_port.pack(side='left')
+
+        # Shared secret key
+        self.txt_secret_key = ScrolledText(self)
+        self.txt_secret_key.config(width=100, height=4)
+        self.txt_secret_key.pack(side='top')
+
+
+        # End Me
         self.quit = tk.Button(self, text='QUIT', fg='red', command=root.destroy)
-        self.quit.pack(side='bottom')
+        self.quit.place(rely=1.0, relx=1.0, x=0, y=0, anchor=tk.SE)
+        self.quit.pack()
         self.refresh_ui()
     
+
     def refresh_ui(self):
-        self.txt_mode.delete('1.0', tk.END)
-        self.txt_mode.insert('1.0', self.state.mode)
+        print('why do i exist')
 
     def say_hi(self):
         print('Hi folks\n')
@@ -45,9 +77,15 @@ class Application(tk.Frame):
     def toggle_mode(self):
         if (self.state.mode == Mode.CLIENT):
             self.state.mode = Mode.SERVER
+            # Disable the IP config
+            self.txt_ip.config(background=root['bg'])
+            self.txt_ip.config(state='disabled')
         else:
             self.state.mode = Mode.CLIENT
-        self.refresh_ui()
+            # Enable the IP config
+            self.txt_ip.config(background='white')
+            self.txt_ip.config(state='normal')
+        self.str_mode.set(self.state.mode)
 
 if __name__ == '__main__':
     root = tk.Tk()
