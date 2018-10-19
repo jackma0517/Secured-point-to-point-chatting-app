90f7664d3e79baeef0e9e8d6ba273261e887339c
valrat <leri.ratu@gmail.com>
2018-10-19 14:24:47 -0700

    added lock to the token

--- py/debug.py
+++ py/debug.py
@@ -1,16 +1,20 @@
 import logging
+import threading
 
 
 class Token():
     def __init__(self):
         self.debug = False
         self.step_next = False
+        self.wait_lock = threading.Lock()
 
 
 def wait_if_debug(token):
+    token.wait_lock.acquire()
     if (token.debug):
         logging.info('Press continue to step')
         while (not token.step_next and token.debug):
             continue
         token.step_next = False
+    token.wait_lock.release()
     return
